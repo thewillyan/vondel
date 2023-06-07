437c4c69b443247109c303a87a53ace1b1dfc89f
taldoflemis <coderflemis@gmail.com>
2023-06-07 17:11:21 -0300

    feat: add clap and InterpreterCLI
    add clap as dependency
    
    add InterpreterCLI
    
    update InterpreterCli to use own version and custom help message

---
+++ src/inter/cli.rs
@@ -0,0 +1,32 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, ValueEnum, Clone)]
+pub enum EvaluatorChoice {
+    /// Use the Rust evaluator for processing the AST
+    Rust,
+    /// Use the Custom Evaluator for processing the AST
+    Custom,
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "Vondel Interpreter")]
+#[command(version = "1.0")]
+#[command(about = "A simple interpreter for the Vondel Language")]
+#[command(author, long_about = None)]
+#[command(
+    help_template = "{author-with-newline} {about-section}Version: {version} \n\n {usage-heading} {usage} \n {all-args} {tab}"
+)]
+pub struct InterpreterCli {
+    /// Which evaluator to use with the Vondel Language
+    #[arg(value_enum)]
+    #[arg(short, long, default_value = "rust")]
+    pub evaluator: EvaluatorChoice,
+
+    /// The file to run the interpreter on
+    #[arg(short, long)]
+    pub file: Option<String>,
+
+    /// The verbose flag
+    #[arg(short, long, default_value = "false")]
+    pub verbose: bool,
+}
