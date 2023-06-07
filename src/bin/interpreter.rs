bf9ee521bc1820590129ce0c32e87ba553bdd33d
taldoflemis <coderflemis@gmail.com>
2023-06-07 17:12:42 -0300

    update interpreter to use clap for decisions

--- src/bin/interpreter.rs
+++ src/bin/interpreter.rs
@@ -1,17 +1,37 @@
-use vondel::inter::repl;
+use anyhow::Result;
+use clap::Parser;
+use vondel::inter::{
+    cli::{EvaluatorChoice, InterpreterCli},
+    evaluator::{self, evaluate_buffer},
+    repl,
+};
+fn read_from_file(file: &str) -> Result<String> {
+    let buf = match std::fs::read_to_string(file) {
+        Ok(buf) => buf,
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            std::process::exit(1);
+        }
+    };
+    Ok(buf)
+}
 
-const WELCOME_MESSAGE: &str = "
-\t██╗   ██╗ ██████╗ ███╗   ██╗██████╗ ███████╗██╗     
-\t██║   ██║██╔═══██╗████╗  ██║██╔══██╗██╔════╝██║     
-\t██║   ██║██║   ██║██╔██╗ ██║██║  ██║█████╗  ██║     
-\t╚██╗ ██╔╝██║   ██║██║╚██╗██║██║  ██║██╔══╝  ██║     
-\t ╚████╔╝ ╚██████╔╝██║ ╚████║██████╔╝███████╗███████╗
-\t  ╚═══╝   ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝
-\t                                                    
-Welcome to the Vondel interpreter!
-Start writing anything!
-";
-fn main() {
-    print!("{WELCOME_MESSAGE}");
-    repl::start();
+fn main() -> Result<()> {
+    let cli = InterpreterCli::parse();
+    let verb = cli.verbose;
+
+    let evaluator: Box<dyn evaluator::Evaluator> = match cli.evaluator {
+        EvaluatorChoice::Rust => Box::new(evaluator::rust::RustEvaluator::new()),
+        EvaluatorChoice::Custom => Box::new(evaluator::custom::CustomEvaluator::new()),
+    };
+
+    match cli.file {
+        Some(file) => {
+            let buf = read_from_file(&file)?;
+            evaluate_buffer(evaluator, buf)?;
+        }
+        None => repl::start(evaluator, verb),
+    }
+
+    Ok(())
 }
