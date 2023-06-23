9d0a6ce36744205be0702c0f80d75e428ea3045a
taldoflemis <coderflemis@gmail.com>
2023-06-22 21:31:41 -0300

    update factorial.asm

--- programs/factorial.asm
+++ programs/factorial.asm
@@ -1,13 +1,12 @@
 .text
 _start:
-  lui a0, 5        ; Number for which factorial is calculated
-  lui a1, 1        ; Initialize a1 to 1 (used as a counter)
-  lui ra, 1        ; Initialize ra to 1 (used to multiply)
+  lui a0 <- 5        ; Number for which factorial is calculated
+  lui a1, ra <- 1    ; Initialize a1 to 1 (used as a counter)
 
 loop:
   beq a1, a0, done   ; If counter equals N, exit the loop
-  mul ra <-  ra, a1  ; Multiply ra by a1
-  addi a1, a1, 1     ; Increment the counter
-  j loop             ; Jump to the loop
+  mul ra <- ra, a1  ; Multiply ra by a1
+  addi a1 <- a1, 1     ; Increment the counter
+  jal loop             ; Jump to the loop
 
 done:
