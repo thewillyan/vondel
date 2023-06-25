a71da43c42611bb8fa64806ebb197ff459097f5d
taldoflemis <coderflemis@gmail.com>
2023-06-25 14:10:08 -0300

    add new assembly that can use hardware impl and div.asm add div.asm
    add div_hardware.asm
    
    add factorial_hardware.asm
    
    add power_hardware.asm

--- programs/csw.asm
+++ programs/csw.asm
@@ -5,14 +5,14 @@
 
 .text
 main:
-  read t0 <- a ; load a into t0
-  read t1 <- b ; load b into t1
-  read t2 <- c ; load c into t2
+  read t0 <- a ; load a into t0, t0 is r1 in uarch
+  read t1 <- b ; load b into t1, t1 is r2 in uarch
+  read t2 <- c ; load c into t2, t2 is r3 in uarch
 
 csw:
   beq t0, t2, a_equals_c ; if a == c, jump to a_equals_c
   mov t0 <- t2 ; a = c 
-  lui ra <- 1 ; ra = 1
+  lui ra <- 1 ; ra = 1, ra is r0 in uarch
   jal done ; jump to done
 
 a_equals_c:
